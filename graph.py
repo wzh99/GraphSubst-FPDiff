ae01678580434872788432d56262e94953363ba4
wzh99 <wzh1999_frog@126.com>
2020-07-24 08:55:44 +0800

    Use rewriting procedure in Relay.

--- graph.py
+++ graph.py
@@ -86,50 +86,13 @@ class _SubstFuncPass:
     def __init__(self, subst: GraphSubst):
         self.subst = subst
 
-    def transform_function(self, func: relay.Function, _mod: ir.IRModule,
+    def transform_function(self, fn: relay.Function, _mod: ir.IRModule,
                            _ctx: transform.PassContext) -> relay.Function:
-        return _SubstMutator(self.subst).visit_function(func)
-
-    def __call__(self, mod: ir.IRModule) -> ir.IRModule: ...
-
-
-class _SubstMutator(relay.ExprMutator):
-    def __init__(self, subst: GraphSubst):
-        super(_SubstMutator, self).__init__()
-        self.subst = subst
-        self.replaced: Dict[relay.Expr, relay.Expr] = {}
-
-    def visit(self, expr: relay.Expr) -> relay.Expr:
-        new_expr = super(_SubstMutator, self).visit(expr)
-        if not self.subst.pattern.match(new_expr):
-            return new_expr
-        new_expr = self.subst(new_expr)
-        self.replaced[expr] = new_expr
-        return new_expr
-
-    def visit_call(self, call: relay.Call) -> relay.Call:
-        new_args = [self._try_find(arg) for arg in call.args]
-        return relay.Call(call.op, new_args, attrs=call.attrs)
-
-    def visit_tuple(self, tup: relay.Tuple) -> relay.Tuple:
-        new_fields = [self._try_find(field) for field in tup.fields]
-        return relay.Tuple(new_fields)
-
-    def visit_tuple_getitem(self, getitem: relay.TupleGetItem) \
-            -> relay.TupleGetItem:
-        new_value = self._try_find(getitem.tuple_value)
-        return relay.TupleGetItem(new_value, getitem.index)
-
-    def _try_find(self, expr: relay.Expr) -> relay.Expr:
-        if self.replaced.__contains__(expr):
-            return self.replaced[expr]
-        else:
-            return self.visit(expr)
-
-    def visit_function(self, fn: relay.Function) -> relay.Function:
-        new_body = self.visit(fn.body)
+        new_body = self.subst.rewrite(fn.body)
         return relay.Function(relay.analysis.free_vars(new_body), new_body,
                               ret_type=fn.ret_type)
+
+    def __call__(self, mod: ir.IRModule) -> ir.IRModule: ...
 
 
 def visualize(wl: Workload, name: Optional[str] = None, path: str = ''):
